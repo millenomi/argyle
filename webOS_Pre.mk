4f3e576bc154d6b1c9ba048ec2fc256930b98714
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2010-05-13 00:01:41 +0200

    Modified webOS Pre platform to use its own ar and ranlib.

--- webOS_Pre.mk
+++ webOS_Pre.mk
@@ -6,3 +6,5 @@ PLATFORM_LIB_PREFIX = lib
 PLATFORM_CXXFLAGS = -I$(PALM_WEBOS_PDK)/include -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -I$(PALM_WEBOS_PDK)/include/SDL --sysroot=$(PALM_WEBOS_PDK)/arm-gcc/sysroot -L$(PALM_WEBOS_PDK)/device/lib -Wl,--allow-shlib-undefined -lSDL -lGLESv2
 
 CXX = $(PALM_WEBOS_PDK)/arm-gcc/bin/arm-none-linux-gnueabi-g++
+AR = $(PALM_WEBOS_PDK)/arm-gcc/bin/arm-none-linux-gnueabi-ar
+RANLIB = $(PALM_WEBOS_PDK)/arm-gcc/bin/arm-none-linux-gnueabi-ranlib
