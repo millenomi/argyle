c88cdfd1583f307e64d9c017b5d86beb537cf8a7
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2010-05-17 00:30:02 +0200

    Build platform for webOS (Palm Pixi).

---
+++ webOS_Pixi.mk
@@ -0,0 +1,10 @@
+PALM_WEBOS_PDK = /opt/PalmPDK
+
+PLATFORM_DYLIB_SUFFIX = .so
+PLATFORM_LIB_PREFIX = lib
+
+PLATFORM_CXXFLAGS = -I$(PALM_WEBOS_PDK)/include -mcpu=arm1136jf-s -mfpu=vfp -mfloat-abi=softfp -I$(PALM_WEBOS_PDK)/include/SDL --sysroot=$(PALM_WEBOS_PDK)/arm-gcc/sysroot -L$(PALM_WEBOS_PDK)/device/lib -Wl,--allow-shlib-undefined -lSDL -lGLESv2
+
+CXX = $(PALM_WEBOS_PDK)/arm-gcc/bin/arm-none-linux-gnueabi-g++
+AR = $(PALM_WEBOS_PDK)/arm-gcc/bin/arm-none-linux-gnueabi-ar
+RANLIB = $(PALM_WEBOS_PDK)/arm-gcc/bin/arm-none-linux-gnueabi-ranlib
